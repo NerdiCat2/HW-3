4ea59e5bf5ae25f79e3ac6cdc78f6dec708a26b0
Matt <mattc@mill5.com>
2024-04-06 14:11:28 -0400

    stuff

--- servicereq/src/App.tsx
+++ servicereq/src/App.tsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// OrderFlowers.tsx
+import React, { useState } from "react";
+import "./App.css";
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+//import { useFormData } from "./useFormData";
+//import {Simulate} from "react-dom/test-utils";
+//import submit = Simulate.submit;
 
-function App() {
-  const [count, setCount] = useState(0)
+const OrderFlowers: React.FC = () => {
+    const [patientName, setPatientName] = useState("");
+    const [patientRoom, setPatientRoom] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    //const { formData, setFormData } = useFormData();
 
-export default App
+    /*const handleReviewOrder = () => {
+        setFormData({
+          ...formData,
+          orderFlowers: {
+            patientName,
+            patientRoom,
+            customMessage,
+          },
+        });
+        navigate("/order-flowers-result");
+      };*/
+
+    async function submit() {
+        if (patientName == "" || patientRoom == "") {
+            alert("Please Fill out the Patient Name and Room Number");
+            return;
+        }
+    }
+
+
+    return (
+        <div className="container">
+            <h2 className="title">Order Medical Equipment</h2>
+            <form>
+                <div className="inputRow">
+                    <div className="formGroup">
+                        <label className="label">Employee Name</label>
+                        <input
+                            className="input"
+                            type="text"
+                            value={patientName}
+                            onChange={(e) => setPatientName(e.target.value)}
+                            placeholder="Add Name Here"
+                        />
+                    </div>
+                    <div className="formGroup">
+                        <label className="label">Destination Room</label>
+                        <input
+                            className="input"
+                            type="number"
+                            value={patientRoom}
+                            onKeyDown={(e) =>
+                                (e.keyCode === 69 || e.keyCode === 190) && e.preventDefault()
+                            }
+                            onChange={(e) => setPatientRoom(e.target.value)}
+                            placeholder="Room Number Here"
+                        />
+                    </div>
+                </div>
+                <div className="formGroup">
+                    <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        defaultValue="female"
+                        name="radio-buttons-group"
+                    >
+                        <FormControlLabel value="female" control={<Radio/>} label="Female"/>
+                        <FormControlLabel value="male" control={<Radio/>} label="Male"/>
+                        <FormControlLabel value="other" control={<Radio/>} label="Other"/>
+                    </RadioGroup>
+                </div>
+
+                <div className="buttonGroup">
+
+                    <button
+                        className={`button.reviewButton`}
+                        type="button"
+                        onClick={submit}
+                    >
+                        Continue
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default OrderFlowers;
